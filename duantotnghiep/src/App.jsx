4aea7440ef1335a27af181990700bdc1fe5f7510
LanPhong <toan2211w1@gmail.com>
2024-09-29 09:37:27 +0700

    new branch

---
+++ duantotnghiep/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Footer from './compoment/Footer'
+import Header from './compoment/Header'
+import Register from './compoment/Register'
+function App() {
+
+  return (
+    <>
+       <Header />
+       <Register />
+       <Footer />
+    
+    </>
+  )
+}
+
+export default App
