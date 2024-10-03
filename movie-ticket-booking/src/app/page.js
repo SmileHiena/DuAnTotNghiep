4308527bf8ac3f4bdc087f752469ae1d4eca98bc
Truonghoai2806 <Truonghoai2806@gmail.com>
2024-10-03 14:12:53 +0700

    zozozo

--- movie-ticket-booking/src/app/page.js
+++ movie-ticket-booking/src/app/page.js
@@ -1,15 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import BlogSection from './component/blog';
 import Event from './component/event';
-import SapChieu from './component/sapchieu';
-import PhimDangChieu from './component/dangchieu';
+// import SapChieu from './component/sapchieu';
+// import PhimDangChieu from './component/dangchieu';
 export default function Home() {
   return (
     <div>
       <Event/>
-      <SapChieu/>
-      <PhimDangChieu/>
+      {/* <SapChieu/> */}
+      {/* <PhimDangChieu/> */}
       <BlogSection />
      
     </div>
