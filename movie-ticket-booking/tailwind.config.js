39132e5897e917f023f6bf7c3144629bed8dbfc0
SmileHiena <nam22042004@gmail.com>
2024-10-12 15:18:37 +0700

    change

---
+++ movie-ticket-booking/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}', // Chỉ định tất cả các file trong thư mục src (hoặc thư mục chính của bạn)
+      './public/**/*.html',         // Chỉ định các file HTML trong thư mục public
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
