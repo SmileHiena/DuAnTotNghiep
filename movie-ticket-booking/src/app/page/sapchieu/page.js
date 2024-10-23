d059143f5193896e4c87ba1d308e1f3486e41990
SmileHiena <nam22042004@gmail.com>
2024-10-23 14:40:44 +0700

    Delete movie-ticket-booking directory

--- movie-ticket-booking/src/app/page/sapchieu/page.js
+++
@@ -1,7 +0,0 @@
-import MovieList from "../../component/MovieList";
-
-const SapChieu = () => {
-    return <MovieList apiUrl="http://localhost:3000/movies?trangThai=dangchieu" title="Phim Sắp Chiếu" />;
-};
-
-export default SapChieu;
