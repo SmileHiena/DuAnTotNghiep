e0eafdf95b5726e36ff0927e35e334d6e7f16529
Truonghoai2806 <Truonghoai2806@gmail.com>
2024-11-14 18:47:23 +0700

    thay doi

--- movie-ticket-booking/src/app/page/dangchieu/page.js
+++
@@ -1,7 +0,0 @@
-import MovieList from "../../component/MovieList";
-
-const DangChieu = () => {
-  return <MovieList apiUrl="http://localhost:3000/movies?trangThai=dangchieu" title="Phim Đang Chiếu" />;
-};
-
-export default DangChieu;
--- movie-ticket-booking/src/app/page/sapchieu/page.js
+++
@@ -1,7 +0,0 @@
-import MovieList from "../../component/MovieList";
-
-const SapChieu = () => {
-    return <MovieList apiUrl="http://localhost:3000/movies?trangThai=sapchieu" title="Phim Sắp Chiếu" />;
-};
-
-export default SapChieu;
